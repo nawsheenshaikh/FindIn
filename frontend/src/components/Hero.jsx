379e0498ae2969019379f787351fd1aa2a7b2a09
nawsheenshaikh <nawsheens56@gmail.com>
2024-10-10 05:43:34 -0700

    first commit

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Find Your Dream Job Today</h1>
+      <h4>
+        Bringing talent and job opportunities together across the country for
+        all skill levels.
+      </h4>
+      <div className="box">
+        Discover a wide variety of job opportunities across different
+        industries. Whether you're an experienced expert or just beginning your
+        career, find the right role to take the next step. Our platform makes
+        job hunting simple and fast, helping you get closer to your next great
+        job.
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
